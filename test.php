68e60c8a0a561b3168810f139ece4c53e2c5693b
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-10 08:19:23 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
-
+<php>
 Name: <input type="text" name="name" value="<?php echo $name;?>">
-
+</br>
 E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-
+</br>
 Website: <input type="text" name="website" value="<?php echo $website;?>">
-
+</br>
 Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-
+</br>
 Gender:
 <input type="radio" name="gender"
 <?php if (isset($gender) && $gender=="female") echo "checked";?>
@@ -17,3 +17,4 @@ value="male">Male
 <input type="radio" name="gender"
 <?php if (isset($gender) && $gender=="other") echo "checked";?>
 value="other">Other
+</php>
