34de3b9bc72b0a3c2f41047a53a2351fe0677f19
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-10 08:27:43 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,27 +1,11 @@
 <php>
 Name: <input type="text" name="name" value="<?php echo $name;?>">
-  
-$file1 = fopen("Zadanie_Ulozene.txt","w");
-  fwrite($file1, $name);
-        fclose($file1);
 </br>
 E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-
-$file2 = fopen("Zadanie_Ulozene.txt","w");
-  fwrite($file2, $email);
-        fclose($file2);
 </br>
 Website: <input type="text" name="website" value="<?php echo $website;?>">
-
-$file3 = fopen("Zadanie_Ulozene.txt","w");
-  fwrite($file3, $website);
-        fclose($file3);
 </br>
 Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-
-$file4 = fopen("Zadanie_Ulozene.txt","w");
-  fwrite($file4, $comment);
-        fclose($file4);
 </br>
 Gender:
 <input type="radio" name="gender"
