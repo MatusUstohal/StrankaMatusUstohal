7ee81b720aeaf04b62432e5f9ac8ad210b9605e2
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-20 13:22:35 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,9 @@
-<p style="text-align:middle">Your information has been submitted successfully.</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<main>
+    <p style="text-align:middle">Your information has been submitted successfully.</p>
+</main>
+</html>
