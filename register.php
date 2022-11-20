a795ccacb69637948bb2c8074bda1ee5d2ea3fbd
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-20 13:54:27 +0100

    Update register.php

--- register.php
+++ register.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="registercss.css">
-<style>
-    h1 {text-align: center;}
-</style>
 </head>
 <main>
     <h1>Your information has been submitted successfully.</h1>
