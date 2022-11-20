d5e136429c2a1fb89d522271d758176ecd03b878
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-20 13:50:52 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,18 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <style>
-    h1 {text-align: center;}
     <link rel="stylesheet" href="registercss.css">
+<style>
+    h1 {text-align: center;}
 </style>
 </head>
 <main>
-    <div class="bg"></div>
-    <div class="bg bg2"></div>
-    <div class="bg bg3"></div>
-    <div class="content">
-    <h1>Sliding Diagonals Background Effect</h1>
-</div>
     <h1>Your information has been submitted successfully.</h1>
 </main>
 </html>
