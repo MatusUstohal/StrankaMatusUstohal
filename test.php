81084faf2c2bad0d8ed432b28ad3b1dae554b809
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-11-20 12:15:21 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,41 @@
-<php>
-Name: <input type="text" name="name" value="<?php echo $name;?>">
-</br>
-E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-</br>
-Website: <input type="text" name="website" value="<?php echo $website;?>">
-</br>
-Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-</br>
-Gender:
-<input type="radio" name="gender"
-<?php if (isset($gender) && $gender=="female") echo "checked";?>
-value="female">Female
-<input type="radio" name="gender"
-<?php if (isset($gender) && $gender=="male") echo "checked";?>
-value="male">Male
-<input type="radio" name="gender"
-<?php if (isset($gender) && $gender=="other") echo "checked";?>
-value="other">Other
-</php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <title>Register</title>
+</head>
+<body>
+<main>
+    <form action="register.php" method="post">
+        <h1>Sign Up</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="password2">Password Again:</label>
+            <input type="password" name="password2" id="password2">
+        </div>
+        <div>
+            <label for="agree">
+                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
+                with the
+                <a href="#" title="term of services">term of services</a>
+            </label>
+        </div>
+        <button type="submit">Register</button>
+        <footer>Already a member? <a href="login.php">Login here</a></footer>
+    </form>
+</main>
+</body>
+</html>
