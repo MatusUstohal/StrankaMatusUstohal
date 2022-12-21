18630ca9502da13dba5024a089b70e8e82f4320c
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 00:31:11 +0100

    Update zadanieESP.php

--- zadanieESP.php
+++ zadanieESP.php
@@ -1,9 +1,14 @@
 <?php
 
-   if(isset($_GET["LCD"]) || isset($_GET["Button"]) || isset($_GET["Light"])) {
-       
+   if(isset($_GET["LCD"]) || isset($_GET["Light"])) {
+      
+      if(isset($_GET["Button"])){
+         $text2 ="Button: 1" . "\n";
+      }else{
+         $text2 ="Button: 0" . "\n";
+      }
+      
       $text1 ="LCD: " . $_GET['LCD'] . "\n";
-      $text2 ="Button: " . $_GET['Button'] . "\n";
       $text3 ="Light(%): " . $_GET['Light'] . "\n";
       $file = fopen("zadanieESP.txt","w") or die("Unable to open file!");
       fwrite($file, $text1);
