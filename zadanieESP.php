3fa93850c7182f867c3056a210410454d3dbbaa5
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-21 23:49:11 +0100

    Update zadanieESP.php

--- zadanieESP.php
+++ zadanieESP.php
@@ -1,14 +1,14 @@
 <?php
 
-    // Below code checks if Name and Age have values. 
-
    if(isset($_GET["LCD text"]) || isset($_GET["Button"]) ) {
        
       $text1 ="LCD text: " . $_GET['LCD text'] . "\n";
       $text2 ="Button: " . $_GET['Button'] . "\n";
+      $text3 ="Light(%): " . $_GET['Light'] . "\n";
       $file = fopen("zadanieESP.txt","w") or die("Unable to open file!");
       fwrite($file, $text1);
       fwrite($file, $text2);
+      fwrite($file, $text3);
       fclose($file);
        
       exit();
@@ -26,6 +26,8 @@
          LCD text: <input type = "text" name = "LCD text" />
 
          Button: <input type = "checkbox" name = "Button" />
+          
+         Light-intensity: <input type="range" min="0" max="100" value="50" class="slider" id="Light">
 
          <input type = "submit" />
 
