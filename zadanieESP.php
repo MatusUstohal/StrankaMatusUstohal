5dd767279483ebde9f546803ed0f8861f543bb3a
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-21 23:35:00 +0100

    Update zadanieESP.php

--- zadanieESP.php
+++ zadanieESP.php
@@ -2,18 +2,14 @@
 
     // Below code checks if Name and Age have values. 
 
-   if(isset($_GET["name"]) || isset($_GET["age"]) ) {
-
-      echo "Hi ". $_GET['name']. "<br />";
-
-      echo "Age: ". $_GET['age']. " years old.";
+   if(isset($_GET["LCD text"]) || isset($_GET["Button"]) ) {
        
-      $text1 ="Name: " . $_GET['name'] . "\n";
-      $text2 ="Age: " . $_GET['age'] . "\n";
+      $text1 ="LCD text: " . $_GET['LCD text'] . "\n";
+      $text2 ="Button: " . $_GET['Button'] . "\n";
       $file = fopen("zadanieESP.txt","w") or die("Unable to open file!");
       fwrite($file, $text1);
       fwrite($file, $text2);
-      fclose($file);   
+      fclose($file);
        
       exit();
 
@@ -27,25 +23,13 @@
 
       <form action = "<?php $_PHP_SELF ?>" method = "GET">
 
-         Name: <input type = "text" name = "name" />
+         LCD text: <input type = "text" name = "LCD text" />
 
-         Age: <input type = "text" name = "age" />
+         Button: <input type = "checkbox" name = "Button" />
 
          <input type = "submit" />
 
       </form>     
-       
-       <!-- Rectangular switch -->
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider"></span>
-</label>
-
-<!-- Rounded switch -->
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label>
 
    </body>
 
