1c67776b3ae2542e5fb7fb166d9d610e876d8b14
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 00:38:18 +0100

    Update zadanieESP.php

--- zadanieESP.php
+++ zadanieESP.php
@@ -59,6 +59,13 @@
 ?>
 
 <html>
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>Register</title>
+   </head>
    <body>
 
       <form action = "<?php $_PHP_SELF ?>" method = "GET">
