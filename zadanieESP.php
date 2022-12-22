b1ba4b9b20e9fe15dbe1aec767973c0d6faf5690
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 01:21:07 +0100

    Update zadanieESP.php

--- zadanieESP.php
+++ zadanieESP.php
@@ -23,7 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
     <link rel="stylesheet" href="styles.css">
-    <title>Register</title>
+    <title>Update</title>
 </head>
 <body>
 <main>
