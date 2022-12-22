94c0e2cc66d8c51a3178437608be1ff8a72d2486
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 01:08:46 +0100

    Update stranka2.php

--- stranka2.php
+++ stranka2.php
@@ -42,29 +42,6 @@
             <label for="username">Username:</label>
             <input type="text" name="username" id="username">
         </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-        </div>
-        <div>
-            <label for="number">Telephone number:</label>
-            <input type="text" name="number" id="number">
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <label for="company">Company:</label>
-            <input type="company" name="company" id="company">
-        </div>
-        <div>
-            <label for="agree">
-                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
-                with the
-                <a href="#" title="terms of services">terms of services</a>
-            </label>
-        </div>
         <button type="submit">Register</button>
     </form>
 </main>
