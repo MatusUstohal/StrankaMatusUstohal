d360dd85444be7c12baa3c58b6c93b26ed2505b7
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 03:11:51 +0100

    Update ESPzadanie.php

--- ESPzadanie.php
+++ ESPzadanie.php
@@ -40,7 +40,7 @@
         </div>
         <div>
             <label for="Light">Light(%):</label>
-            <input type="range" name="Light" id="Light" min="0" max="100" value="50" oninput="TextL.rangeValue.innerText = this.value">
+            <input type="range" name="Light" id="Light" min="0" max="100" value="50" oninput="rangeValue.innerText = this.value">
             <p>Your chosen light level is at: </p>
             <p id="rangeValue">50</p>
             <span id="innerText">%</span>
