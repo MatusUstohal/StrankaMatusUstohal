a511a3703a8e4918ff2f3e5cf6d47e6d5e8bf3ac
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-22 02:50:44 +0100

    Update ESPzadanie.php

--- ESPzadanie.php
+++ ESPzadanie.php
@@ -40,12 +40,8 @@
         </div>
         <div>
             <label for="Light">Light(%):</label>
-            <input type="range" name="Light" id="Light" min="0" max="100" value="50" oninput="rangeValue.innerText = this.value">
-            <ul>
-                <li>You are currently on:</li>
-                <li><p id="rangeValue">50</p></li>
-                <li>%</li>
-            </ul>
+            <input type="range" name="Light" id="Light" min="0" max="100" value="50" oninput="rangeValue.innerText = this.value" . "%">
+            <p You are currently on: id="rangeValue">50</p>
         </div>
         <button type="submit">Update</button>
     </form>
