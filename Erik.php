df04c3b27486373d477a4c5f9c0898076e3ff9f7
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2022-12-23 20:55:09 +0100

    Update Erik.php

--- Erik.php
+++ Erik.php
@@ -1,12 +1,15 @@
-<!--<?php
+<?php
+if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email']) && isset($_POST['number']) && isset($_POST['password']) && isset($_POST['company']) && isset($_POST['username'])){
     $text1 ="Name: " . $_POST['name'] . "\n";
     $text2 ="Surname: " . $_POST['surname'] . "\n";
-    $text3 ="Alias: " . $_POST['username'] . "\n";
-    $text4 ="Email: " . $_POST['email'] . "\n";
-    $text5 ="Telephone number: " . $_POST['number'] . "\n";
-    $text6 ="Password: " . $_POST['password'] . "\n";
-    $text7 ="Company: " . $_POST['company'];
-    $file = fopen("zadanie.txt","w") or die("Unable to open file!");
+    $text3 ="Email: " . $_POST['email'] . "\n";
+    $text4 ="Telephone number: " . $_POST['number'] . "\n";
+    $text5 ="Password: " . $_POST['password'] . "\n";
+    $text6 ="Company: " . $_POST['company'];
+    $text7 ="Alias: " . $_POST['username'] . "\n";
+
+    $file = fopen("Erik.txt","w") or die("Unable to open file!");
+
     fwrite($file, $text1);
     fwrite($file, $text2);
     fwrite($file, $text3);
@@ -14,8 +17,10 @@
     fwrite($file, $text5);
     fwrite($file, $text6);
     fwrite($file, $text7);
-    fclose($file);   
-?>-->
+
+    fclose($file); 
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -45,6 +50,26 @@
         <input id="email" class="input" type="text" placeholder=" " />
         <div class="cut cut-short"></div>
         <label for="email" class="placeholder">Email</>
+      </div>
+      <div class="input-container ic2">
+        <input id="number" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="number" class="placeholder">Telephone number</>
+      </div>
+      <div class="input-container ic2">
+        <input id="password" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="password" class="placeholder">Password</>
+      </div>
+      <div class="input-container ic2">
+        <input id="company" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="company" class="placeholder">Company name</>
+      </div>
+      <div class="input-container ic2">
+        <input id="username" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="username" class="placeholder">Username</>
       </div>
       <button type="text" class="submit">submit</button>
     </div>
