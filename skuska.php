5c88fad31677732b70d56ae51ad84d0084291ee5
MatusUstohal <114644410+MatusUstohal@users.noreply.github.com>
2023-01-26 08:37:29 +0100

    Update skuska.php

--- skuska.php
+++ skuska.php
@@ -46,22 +46,7 @@
             <label for="number">Telephone number:</label>
             <input type="text" name="number" id="number">
         </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <label for="company">Company:</label>
-            <input type="company" name="company" id="company">
-        </div>
-        <div>
-            <label for="agree">
-                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
-                with the
-                <a href="#" title="terms of services">terms of services</a>
-            </label>
-        </div>
-        <button type="submit">Register</button>
+        <button type="submit">Submit</button>
     </form>
 </main>
 </body>
